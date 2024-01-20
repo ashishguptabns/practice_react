b9fe5f08fb374a10bfc151f3e0dffc975e093403
ashishguptabns <ashishguptabns@gmail.com>
2024-01-20 17:50:20 +0530

    theme

--- src/app/page.js
+++ src/app/page.js
@@ -9,20 +9,23 @@ import { Counter } from './components/counter/counter'
 import { Carousel } from './components/carousal/carousal'
 import people from './components/carousal/data'
 import Link from "next/link";
+import useTheme from "./hooks/useTheme";
 
 export default function Home() {
 
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [theme, toggleTheme] = useTheme()
 
   return (
     <div className={styles.main}>
       <div className={styles.center}>
-        <Link href={`/todo/`}>
-          <button className={styles.modalBtn}>Todo</button>
-        </Link>
         {
           !isModalOpen &&
           <>
+            <button className={styles.modalBtn} onClick={toggleTheme}>Toggle Theme</button>
+            <Link href={`/todo/`}>
+              <button className={styles.modalBtn}>Todo</button>
+            </Link>
             <button className={styles.modalBtn} onClick={() => setIsModalOpen(true)}>Shared Modal</button>
             <WindowConfirm />
             <Counter />
